71ef6069301700bfac71bf2a69a068607179f237
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-11 20:25:53 -0500

    index.js add

---
+++ index.js
@@ -0,0 +1,15 @@
+// Arrays methods Javascript
+// 1. ForEach
+const array = ["a", "b", "c"]
+array.forEach(function(item){
+    console.log(item);
+});
+
+// 2. .map
+
+const array1 = [ 1, 4, 6, 8];
+const map = array1.map((num) => num * 3);
+console.log(map)
+
+// . filter
+
