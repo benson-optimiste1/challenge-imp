4298766158d8789c4a2eddabea20145ec5fe3d18
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-17 11:17:40 -0500

    something add

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ console.log(map)
 
 // . filter
 
+//. solution 3
+products.map(product => {
+    product.onSale = (product.quantity > 100)
+})
+
+// .Given an array of products, where each products, where each product is an object containing a unique Sku property, a prrice, and a quantity, alter the original array to add an onState : true property to any product with a quantity greater than 100
+
+/**
+ * also return an array called toReorder that contains all the products
+ * that have a quantity of 10 or less.
+ */
+ 
